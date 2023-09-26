3ffe7631b85a668f71a210d6881c6e185ff4598d
후디 이야기Hoodie’s Story <shinm93@gmail.com>
2023-09-27 00:10:31 +0900

    add videoConponent

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import { PageComponent } from './components/page/page.js';
 import { ImageComponent } from './components/page/item/images.js';
 import { NoteComponent } from './components/page/item/note.js';
 import { TodoComponent } from './components/page/item/todo.js';
+import { VideoComponent } from './components/page/item/video.js';
 
 class APP {
   private readonly page: PageComponent;
@@ -13,12 +14,14 @@ class APP {
     const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300')
     image.attachTo(appRoot, 'beforeend')
 
+    const video = new VideoComponent('Video Title', 'https://www.youtube.com/watch?v=Z0XdgGCafI4')
+    video.attachTo(appRoot, 'beforeend')
+
     const note = new NoteComponent('Note Title', 'Note body')
     note.attachTo(appRoot, 'beforeend')
 
     const todo = new TodoComponent('Todo Title', 'Todo Item')
     todo.attachTo(appRoot, 'beforeend')
-
 
 
   }
---
+++ src/components/page/item/video.ts
@@ -0,0 +1,33 @@
+import { BaseComponent } from "../../component.js";
+
+export class VideoComponent extends BaseComponent<HTMLElement> {
+  constructor(title: string, url: string) {
+    super(`<section class="video">
+            <div class="video__player"><iframe class="video__iframe"></iframe></div>
+            <h3 class="video__title"></h3>
+          </section>`)
+
+    const iframe = this.element.querySelector('.video__iframe')! as HTMLIFrameElement
+    // url -> videoId -> embed
+    iframe.src = this.convertToEmbeddedURL(url)
+
+
+    const titleElement = this.element.querySelector('.video__title')! as HTMLHeadingElement
+    titleElement.textContent = title
+
+  }
+
+  private convertToEmbeddedURL(url: string): string {
+    // !!정규표현식 Regex
+    const regExp = /^(?:https?:\/\/)?(?:www\.)?(?:(?:youtube.com\/(?:(?:watch\?v=)|(?:embed\/))([a-zA-Z0-9-]{11}))|(?:youtu.be\/([a-zA-Z0-9-]{11})))/
+    const match = url.match(regExp)
+
+    const videoId = match ? match[1] || match[2] : undefined
+    if (videoId) return `https://www.youtube.com/embed/${videoId}`
+
+    return url
+  }
+}
+
+
+/* <iframe width="950" height="534" src="https://www.youtube.com/embed/b-MyiR3HUzo" title="일본생활 | 군마에서의 여유로운 일상 群馬でのゆとりがある日常" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */
