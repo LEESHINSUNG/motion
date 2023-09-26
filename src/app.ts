ee3e18a5f4b525dc88e8d4b8b23c63e9eef87397
후디 이야기Hoodie’s Story <shinm93@gmail.com>
2023-09-26 23:34:25 +0900

    noteComponent & todoComponent

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 // 주의 확장자 붙이기
 import { PageComponent } from './components/page/page.js';
 import { ImageComponent } from './components/page/item/images.js';
+import { NoteComponent } from './components/page/item/note.js';
+import { TodoComponent } from './components/page/item/todo.js';
 
 class APP {
   private readonly page: PageComponent;
@@ -8,10 +10,19 @@ class APP {
     this.page = new PageComponent()
     this.page.attachTo(appRoot)
 
-    const image = new ImageComponent('Image Title','https://picsum.photos/600/300')
-    image.attachTo(appRoot,'beforeend')
+    const image = new ImageComponent('Image Title', 'https://picsum.photos/600/300')
+    image.attachTo(appRoot, 'beforeend')
+
+    const note = new NoteComponent('Note Title', 'Note body')
+    note.attachTo(appRoot, 'beforeend')
+
+    const todo = new TodoComponent('Todo Title', 'Todo Item')
+    todo.attachTo(appRoot, 'beforeend')
+
+
+
   }
-  
+
 }
 
 new APP(document.querySelector('.document')! as HTMLElement)
